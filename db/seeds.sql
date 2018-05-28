6d4aab3951f68ecfe5330f1314e06e134d39448f
AlCast <alvaro.castelan.21@gmail.com>
2018-05-28 13:56:16 -0500

    added connection + orm

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Da Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
