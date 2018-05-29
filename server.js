e7686e3c440e27cfeea0c967fbf422d53015c7c8
AlCast <alvaro.castelan.21@gmail.com>
2018-05-28 22:32:53 -0500

    got update to work

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var methodOverride = require("method-override");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
@@ -9,8 +9,9 @@ var app = express();
 app.use(express.static("public"));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(methodOverride("_method"));
 // parse application/json
 app.use(bodyParser.json());
 
